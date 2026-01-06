3316a7cc4bac171bd3b0624e09f59a5b7f796f04
Walid02M <97094697+Walid02M@users.noreply.github.com>
2026-01-06 03:13:24 +0100

    Lägger till todos med POST

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,34 +1,32 @@
-// enkel "fake-databas" i minnet för att lära oss CRUD
+// enkel "fake-databas" i minnet
 let todos = [
-{ id: 1, text: "Handla mjölk", done: false },
-{ id: 2, text: "Plugga CRUD", done: false },
+  { id: 1, text: "Handla mjölk", done: false },
+  { id: 2, text: "Plugga CRUD", done: false },
 ];
 
 // GET: hämtar alla todos
 const getTodos = (req, res) => {
-    res.json(todos);
+  res.json(todos);
 };
 
-// POST: Lägger till en ny todo
+// POST: lägger till en ny todo
 const addTodo = (req, res) => {
-    //tar text från body (det vi skickar in)
-    const text = req.body.text;
+  const text = req.body?.text;
 
-    // enkel koll så man inte skickar tom text
-    if (!text) {
-        return res.status(400).json({ message: "Du måste skicka med text" });
-    }
 
-    const newTodo = {
-        id: todos.length + 1, // enkel id-lösning (räcker för nu)
-        text: text,
-        done: false,
-        };
+  // enkel koll så text finns
+  if (!text) {
+    return res.status(400).json({ message: "Du måste skicka med text" });
+  }
 
-        todos.push(newTodo);
-        res.status(201).json(newTodo); // 201= skapad
-    };
+  const newTodo = {
+    id: todos.length + 1,
+    text: text,
+    done: false,
+  };
+
+  todos.push(newTodo);
+  res.status(201).json(newTodo); // 201 = skapad
+};
 
 module.exports = { getTodos, addTodo };
-
-
